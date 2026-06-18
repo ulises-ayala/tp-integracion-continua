98ef3a75e6bc11fad07ee4f6f227dfeb3f9c9965
ulises-ayala <ulisesayala155@gmail.com>
2026-06-18 16:04:03 -0300

    error spectral

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def index()
+def index():
     modelo = request.args.get('model', 'dashboard')
     
     # Es para que siempre entre en el dashboard si esta mal la URL
