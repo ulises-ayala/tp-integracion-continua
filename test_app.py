cb03986a5b0c9e3b06d9885157fd1416464002fc
ulises-ayala <ulisesayala155@gmail.com>
2026-06-18 15:59:11 -0300

    mas errores

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_hello_world():
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 404
+    assert response.status_code == 200
